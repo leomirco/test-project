b4f166a1c935bfa0c3fc177269e5b0531207e7b3
EC2 Default User <ec2-user@ip-172-31-34-91.us-east-2.compute.internal>
2019-12-26 14:49:36 +0000

    umidita casa commit

--- api/controllers/ProductController.js
+++ api/controllers/ProductController.js
@@ -16,7 +16,7 @@ module.exports = {
             var day = new Date().getDate();
             var month = new Date().getMonth()+1;
             var year = "2019";
-            var hour = new Date().getHours();
+            var hour = new Date().getHours()+1;
             var minute = new Date().getMinutes();;
             var second = new Date().getSeconds();
             var data =  day+"-"+month+"-"+year;
@@ -60,7 +60,7 @@ module.exports = {
         return res.badRequest(`product ${req.params.data_rivelazione} not found`);
     },
      getIntentReply: async function(req, res) {
-        console.log("entrato");
+        console.log("LETTURA DATI QUALITA ARIA");
         console.log("Webhook. Request body: "+req);
         console.log("Webhook. Request body: "+res);
         const agent = new WebhookClient({request: req, response: res});
@@ -68,6 +68,7 @@ module.exports = {
         console.log('Webhook. agent.intent: '+df_intent);
         console.log('Webhook. agent.session: '+agent.session);
         if (df_intent === "temperatura casa") {
+            console.log("LETTURA DATI TEMPERATURA");
             // eseguo query di ricerca dell'ultima temperatura rilevata
             var day = new Date().getDate();
             var month = new Date().getMonth()+1;
@@ -82,6 +83,24 @@ module.exports = {
             const session = agent.session;
             var df_res = {};
             df_res['fulfillmentText'] = "la temperatura è di " + temp + " gradi - < "+product[0].data_rivelazione+" / "+ product[0].ora_rivelazione+" >"
+            res.status(200).send(JSON.stringify(df_res));
+        }
+        if (df_intent === "umidita casa") {
+            console.log("LETTURA DATI UMIDITA");
+            // eseguo query di ricerca dell'ultima temperatura rilevata
+            var day = new Date().getDate();
+            var month = new Date().getMonth()+1;
+            var year = "2019";
+            var dataCurrent =  day+"-"+month+"-"+year;
+            console.log("Data corrente: ", dataCurrent);
+            let product = await Product.find({data_rivelazione: dataCurrent}).sort({'createdAt': -1});
+            console.log("prodotto: ", product);
+            var hum = product[0].humidity;
+            console.log('temperatura rilevata: '+product[0].humidity);
+            console.log('intent: '+agent.intent);
+            const session = agent.session;
+            var df_res = {};
+            df_res['fulfillmentText'] = "l'umidità è di " + hum + " % - < "+product[0].data_rivelazione+" / "+ product[0].ora_rivelazione+" >"
             res.status(200).send(JSON.stringify(df_res));
         }
      }
