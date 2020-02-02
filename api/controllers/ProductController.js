1398f0e73525e61c4593de32634389a502a2f947
EC2 Default User <ec2-user@ip-172-31-34-91.us-east-2.compute.internal>
2020-02-02 18:32:29 +0000

    inquinamento indoor 5

--- api/controllers/ProductController.js
+++ api/controllers/ProductController.js
@@ -178,11 +178,12 @@ module.exports = {
             res.on('error', function(e) {
                 console.error(e);
             });
-        }).end();
-         var df_res = {};
+             var df_res = {};
          df_res['fulfillmentText'] = "Valori particolato indoor PM2.5: " + object["info.aqi"].data.pm25 + " PM10: "+object["info.aqi"].data.pm10+" /  >"
          console.log("Valori particolato indoor PM2.5: " + object["info.aqi"].data.pm25 + " PM10: "+object["info.aqi"].data.pm10+" /  >");
          res.status(200).send(JSON.stringify(df_res));
+        }).end();
+        
         }
      },
     
